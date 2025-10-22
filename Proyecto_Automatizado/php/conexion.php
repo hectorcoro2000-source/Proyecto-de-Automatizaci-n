e1b6e5742e7bd190f898798e2168f7be07ac5254
hectorcoro2000-source <hectorcoro2000@gmail.com>
2025-10-22 09:37:50 -0700

    Update conexion.php

--- Proyecto_Automatizado/php/conexion.php
+++ Proyecto_Automatizado/php/conexion.php
@@ -1,10 +1,21 @@
 <?php
+
+// function conexionBD_Localhost($bd){
+// 	$server = 'localhost';
+// 	$user = 'postgres';
+// 	$pass = '1';
+// 	$bd = $bd;
+// 	$connec = pg_connect("host=$server dbname=$bd user=$user password=$pass") or die ("Error de conexion servidor ".$server);
+// 	return $connec;    
+// }
+
 function conexionBD_Localhost($bd){
-	$server = 'localhost';
-	$user = 'postgres';
-	$pass = '1';
+	$server = 'dpg-d3sgeoi4d50c738r9j00-a';
+	$user = 'alumnos_user';
+	$pass = 'eOrXm9DVTwpzWxpvLI1P9bj6tyRFpRCp';
 	$bd = $bd;
 	$connec = pg_connect("host=$server dbname=$bd user=$user password=$pass") or die ("Error de conexion servidor ".$server);
 	return $connec;    
 }
+
 ?>
